22b598c64a4a9a4ff2dcbf1e475d69cbfe0c0880
Nadine Puetzer <nadine.puetzer1@vodafone.com>
2023-07-12 09:06:19 +0200

    Update process_data.py template

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,16 +1,67 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Input: Messages datafile and categories datafile
+    Output: Messages and categories dataframe merged together (df)
+    '''
 
+    #Load messages dataset
+    messages = pd.read_csv(messages_filepath)  
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)  
+    
+    # merge datasets
+    df = messages.merge(categories, how='outer', on=['id'])
+    
+    return df
 
 def clean_data(df):
-    pass
+    '''
+    Data Cleaning Steps:
+    - Split `categories` into separate category columns.
+    - Convert category values to just numbers 0 or 1.
+    - Replace `categories` column in `df` with new category columns.
+    - Remove duplicates.
+    '''
 
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand = True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # extract a list of new column names for categories.
+    category_colnames = row.apply(lambda x: x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1.
+    # set each value to be the last character of the string
+    for column in categories:
+        categories[column] = categories[column].str[-1:]
+    
+    # convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df=df.drop(['categories'], axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories],axis=1)
+    
+    # drop duplicates
+    df=df.drop_duplicates()
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///database_filename')
+    df.to_sql('Clean_Disaster_Data', engine, index=False)  
 
 
 def main():
